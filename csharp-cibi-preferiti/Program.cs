78a5189ba869dfc560f6700d6e96d8d94bc31dfe
Yari <narducciyarigmail.com>
2023-04-04 15:20:30 +0200

    aggiunta ciclo for

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -2,3 +2,8 @@
 string[] cibiPreferiti = { "Hamburger", "Spaghetti alle vongole", "Uova all'occhio di bue", "Fettine Panate", "Patate Lesse" };
 
 Console.WriteLine(cibiPreferiti.Length);
+
+for(var i=0; i<cibiPreferiti.Length; i++)
+{
+    System.Console.WriteLine(cibiPreferiti[i]);
+}
